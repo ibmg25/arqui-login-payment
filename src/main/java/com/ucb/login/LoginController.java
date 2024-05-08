7c209d2aeefcbfebbee026dc20f5e3a44e9f97ef
Ignacio <ignacio.martinez@ucb.edu.bo>
2024-05-08 09:34:38 -0400

    correcciones

--- src/main/java/com/ucb/login/LoginController.java
+++ src/main/java/com/ucb/login/LoginController.java
@@ -2,29 +2,11 @@ package com.ucb.login;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.networknt.schema.JsonSchema;
-import com.networknt.schema.JsonSchemaFactory;
-import com.networknt.schema.SpecVersion.VersionFlag;
-import com.networknt.schema.ValidationMessage;
-import com.ucb.login.dto.UserDto;
-
-import io.sentry.Sentry;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.Set;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
-import com.ucb.login.dto.ErrorResponse;
 import com.ucb.login.dto.LoginRequestDto;
 import com.ucb.login.dto.LoginResponseDto;
 
--- src/main/java/com/ucb/login/PaymentController.java
+++ src/main/java/com/ucb/login/PaymentController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ucb.login.dto.ErrorResponse;
 import com.ucb.login.dto.PaymentDto;
 import com.ucb.login.dto.PaymentResponseDto;
 
@@ -15,11 +14,7 @@ public class PaymentController implements IPaymentApi {
     @Override
     @PostMapping("/api/payment")
     public ResponseEntity<PaymentResponseDto> processPayment(@RequestBody PaymentDto paymentRequest) {
-        // Aquí implementa la lógica para procesar el pago utilizando los datos de la tarjeta de crédito
-        // Si hay errores, puedes devolver un ErrorResponse adecuado
-        // Si el pago se procesa correctamente, devuelve un PaymentResponseDto con los detalles del pago
-
-        // Por ahora, devolvemos una respuesta vacía para el ejemplo
+        
         PaymentResponseDto responseDto = new PaymentResponseDto();
         return ResponseEntity.ok(responseDto);
     }
